b77b88b4e6e2730fa89104428df4da021ded10ea
pavlo606 <pavlyk.dykyy@gmail.com>
2023-05-12 17:17:53 +0300

    add getters, setters and docstrings

--- Insects/Insect.py
+++ Insects/Insect.py
@@ -1,36 +1,92 @@
-class Insect:
-    name: str
-    numberOfLegs: int
-    has_wings: bool
-    is_dangerous: bool
-    is_sleeping: bool
+"""Insect class"""
 
-    def __init__(self, name: str, numberOfLegs: int, has_wings: bool, is_dangerous: bool, is_sleeping: bool) -> None:
-        self.name = name
-        self.numberOfLegs = numberOfLegs
-        self.has_wings = has_wings
-        self.is_dangerous = is_dangerous
-        self.is_sleeping = is_sleeping
+class Insect:
+    """
+    Insect
+    """
+    __name: str
+    __number_of_legs: int
+    __has_wings: bool
+    __is_dangerous: bool
+    __is_sleeping: bool
+
+    def __init__(self, name: str, number_of_legs: int, \
+                 has_wings: bool, is_dangerous: bool, is_sleeping: bool) -> None:
+        self.__name = name
+        self.__number_of_legs = number_of_legs
+        self.__has_wings = has_wings
+        self.__is_dangerous = is_dangerous
+        self.__is_sleeping = is_sleeping
+
+    @property
+    def name(self) -> str:
+        """Getter, Setter"""
+        return self.__name
+
+    @name.setter
+    def name(self, name) -> None:
+        self.__name = name
+
+    @property
+    def number_of_legs(self) -> int:
+        """Getter, Setter"""
+        return self.__number_of_legs
+
+    @number_of_legs.setter
+    def number_of_legs(self, number_of_legs) -> None:
+        self.__number_of_legs = number_of_legs
+
+    @property
+    def has_wings(self) -> int:
+        """Getter, Setter"""
+        return self.__has_wings
+
+    @has_wings.setter
+    def has_wings(self, has_wings) -> None:
+        self.__has_wings = has_wings
+
+    @property
+    def is_dangerous(self) -> bool:
+        """Getter, Setter"""
+        return self.__is_dangerous
+
+    @is_dangerous.setter
+    def is_dangerous(self, is_dangerous) -> None:
+        self.__is_dangerous = is_dangerous
+
+    @property
+    def is_sleeping(self) -> bool:
+        """Getter, Setter"""
+        return self.__is_sleeping
+
+    @is_sleeping.setter
+    def is_sleeping(self, is_sleeping) -> None:
+        self.__is_sleeping = is_sleeping
 
     def is_poisonous(self) -> bool:
+        """
+        Return if insect is poisonous
+        """
         return self.is_dangerous
-    
+
     def hibernate(self) -> None:
+        """
+        Makes insect sleep
+        """
         self.is_sleeping = True
 
     def wake_up(self) -> None:
+        """
+        Makes insect wake up
+        """
         self.is_sleeping = False
 
+    def __repr__(self) -> str:
+        attributes = self.__dict__
+        return f"{attributes}"
+
     def __str__(self) -> str:
-        attributes = list(self.__annotations__.keys())
-        result = f"{self.__class__.__name__}("
-
-        for attribute in attributes:
-            if attributes.index(attribute) == 0:
-                result += f"{attribute}={self.__getattribute__(attribute)}"
-                continue
-            result += f", {attribute}={self.__getattribute__(attribute)}"
-
-        result += ")"
-
-        return result
+        return f"{self.__class__.__name__}(name: {self.name}, " \
+                + f"number of legs: {self.number_of_legs}, has wings: {self.has_wings}, " \
+                + f"is dangerous: {self.is_dangerous}, is sleeping: {self.is_sleeping})"
+    
--- Insects/__main__.py
+++ Insects/__main__.py
@@ -1,4 +1,7 @@
-from Insects.Insect import Insect
+"""Insects"""
+
+from Insects.insect import Insect
 
 insect = Insect("Hornet", 6, True, True, True)
+
 print(insect)
